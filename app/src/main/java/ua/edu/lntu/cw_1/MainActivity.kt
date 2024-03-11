4c6ea2f0798ea83bcee8efdecb0d7dbf655cd76d
xm1ls <xm1ls2000@gmail.com>
2024-03-11 14:48:12 +0200

    implement basic layout without modifiers

--- app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
@@ -3,13 +3,20 @@ package ua.edu.lntu.cw_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw_1.ui.theme.IPZ_CW_1_Hmilovskij_OleksandrTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +38,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column() {
+        Column() {
+            Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
+            Column() {
+                Text(text = "Name Surname")
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(text = "Title")
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Column() {
+                Row() {
+                    Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
+                    Text(text = "number phone")
+                }
+                Row() {
+                    Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
+                    Text(text = "name")
+                }
+                Row() {
+                    Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
+                    Text(text = "mail")
+                }
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
